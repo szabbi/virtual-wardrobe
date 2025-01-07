19011c1c6838b3bc1251e604f5f6259cd9360baf
Nagy Szabolcs <nagy.szabolcsffs@gmail.com>
2025-01-07 20:48:54 +0100

    initial commit

---
+++ server/src/main/java/hu/unideb/inf/virtualwardrobe/VirtualwardrobeApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.virtualwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualwardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualwardrobeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/hu/unideb/inf/virtualwardrobe/VirtualwardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.virtualwardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualwardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
